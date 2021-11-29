8535cb609930baccae617927f8357cf2d393ab2e
JoseIgnacioTapia <josignaciot@gmail.com>
2021-11-29 19:39:13 -0300

    Agregando, completando y borrando Inputs - Realizando las operaciones

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useRef, useState } from 'react';
+import InvoiceLineItems from './components/InvoiceLineItems';
+
+const lineItem = (description = '', quantity = 1, unitPrice = 1) => ({
+  description,
+  quantity,
+  unitPrice,
+});
+
 function App() {
+  const [lineItems, setLineItems] = useState([lineItem()]);
+
+  const addLineItem = () => {
+    setLineItems([...lineItems, lineItem()]);
+  };
+
   return (
     <div>
       <h1>Factura</h1>
+      <InvoiceLineItems lineItems={lineItems} setLineItems={setLineItems} />
+
+      <button type="button" onClick={addLineItem}>
+        Agregar Item
+      </button>
     </div>
   );
 }
---
+++ src/components/InvoiceLineItems.js
@@ -0,0 +1,88 @@
+const InvoiceLineItems = ({ lineItems, setLineItems }) => {
+  const handleLineItemChange = (event, index) => {
+    setLineItems([
+      ...lineItems.slice(0, index),
+      {
+        ...lineItems[index],
+        [event.target.name]: event.target.value,
+      },
+      ...lineItems.slice(index + 1),
+    ]);
+  };
+
+  const removeLineItem = index => {
+    setLineItems([...lineItems.slice(0, index), ...lineItems.slice(index + 1)]);
+  };
+
+  return (
+    <div>
+      <div>
+        <tr>
+          <span>
+            <label>Cantidad</label>
+          </span>
+          <span>
+            <label>Descripción</label>
+          </span>
+          <span>
+            <label>Precio Unitario</label>
+          </span>
+          <span>
+            <label>Importe</label>
+          </span>
+        </tr>
+      </div>
+      <div>
+        {lineItems.map((lineItem, index) => {
+          const { description, unitPrice, quantity } = lineItem;
+
+          const importe = unitPrice * quantity;
+
+          return (
+            <div key={`invoiceLine-${index}`}>
+              <span>
+                <input
+                  name="quantity"
+                  type="number"
+                  value={quantity}
+                  onChange={event => {
+                    handleLineItemChange(event, index);
+                  }}
+                />
+              </span>
+              <span>
+                <input
+                  name="description"
+                  type="text"
+                  value={description}
+                  onChange={event => {
+                    handleLineItemChange(event, index);
+                  }}
+                  type="text"
+                />
+              </span>
+              <span>
+                <input
+                  name="unitPrice"
+                  type="number"
+                  vaue={unitPrice}
+                  onChange={event => {
+                    handleLineItemChange(event, index);
+                  }}
+                />
+              </span>
+              <span>{importe}</span>
+              {lineItems.length > 1 && (
+                <button type="button" onClick={() => removeLineItem(index)}>
+                  X
+                </button>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default InvoiceLineItems;
